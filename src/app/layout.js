c520e05947964de9b2bba34bf949b9ee3ac360cb
Michal Szyszlo <mszyszlo@student.agh.edu.pl>
2025-09-30 13:31:18 +0200

    Footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Navbar from "../components/navbar/Navbar.jsx";
+import Footer from "../components/footer/Footer.jsx";
 
 
 export const metadata = {
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
       <body>
       <Navbar/>
         {children}
+      <Footer />
       </body>
     </html>
   );
