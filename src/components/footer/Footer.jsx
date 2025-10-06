798d5a4fdd16f35f0fbf895579f6274be32afd87
snappify <snappifyacc@gmail.com>
2025-10-06 19:58:52 +0200

    baner fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,8 @@
 import {useState} from "react";
 import {motion} from "motion/react";
 import Link from "next/link";
-import godlo from "src/images/godlo.webp"
+import godlo from "src/images/godlo.webp";
+import logo from "src/images/logo.webp";
 import Image from "next/image";
 
 const PHONE_NUMBER = "+48 84 639 28 01";
@@ -118,7 +119,7 @@ const Footer = () => {
 
                 <div className="w-full h-max flex items-center justify-center py-2">
                     <div className="max-w-[80%] w-full h-max flex items-center justify-center gap-5 py-2">
-                        <motion.img alt="logo" className="select-none h-15 w-15   drop-shadow-lg/20" whileHover={{scale: 1.1}} whileTap={{scale: 1}}/>
+                        <Image src={logo} alt="logo" className="select-none h-15 w-15   drop-shadow-lg/20" />
                         <p className=" text-center text-gray-200 font-[poppins] text-xs sm:text-xs lg:text-md max-w-2/3 font-light whitespace-normal break-words hyphens-auto">I Liceum Ogólnokształcące im. Jana Zamoyskiego w Zamościu </p>
 
                         <Image src={godlo} alt="Godło" className=" select-none h-15 w-auto object-contain" />
--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -9,11 +9,14 @@ export default function Banner() {
             <div className="relative w-full h-max md:h-max flex flex-col pt-[6rem] lg:pt-[7rem] items-center font-[poppins]">
                 <div className="relative w-[94%] sm:w-[90%] lg:w-[80%] h-max flex items-center">
                     <div className="w-full h-max flex items-center gap-5 py-1">
-                        <Image src={logo} width={100} alt="logo" className="select-none max-h-20 object-contain md:max-h-25 lg:max-h-30 min-w-11 drop-shadow-lg/20"/>
-                        <p className="text-md/7 sm:text-lg/7 md:text-xl/9 lg:text-2xl/10 xl:text-3xl/12 max-w-2/3 font-light select-none">I Liceum Ogólnokształcące im. Jana Zamoyskiego w Zamościu </p>
-                    </div>
+                        <Image src={logo} alt="logo" className="select-none max-h-20 object-contain md:max-h-25 lg:max-h-30 min-w-11 drop-shadow-lg/20"/>
+                        {/*<p className="text-md/7 sm:text-lg/7 md:text-xl/9 lg:text-2xl/10 xl:text-3xl/12 max-w-2/3 font-light select-none">I Liceum Ogólnokształcące im. Jana Zamoyskiego w Zamościu </p>*/}
+	                      <p className=" text-center text-black  font-[poppins] text-sm/7 sm:text-md/7 md:text-lg/9 lg:text-xl/10 xl:text-2xl/12 font-semibold whitespace-normal break-words hyphens-auto">I Liceum Ogólnokształcące im. Jana Zamoyskiego w Zamościu </p>
+
+
                     <div className="max-h-30">
-                        <Image src={godlo} priority width={100} alt="Godło" className="object-contain select-none max-h-20 md:max-h-25 lg:max-h-30" />
+                        <Image src={godlo} priority alt="Godło" className="object-contain select-none max-h-20 md:max-h-25 lg:max-h-30 min-w-11" />
+                    </div>
                     </div>
                 </div>
                 <div className="relative w-[94%] sm:w-[90%] lg:w-[80%] md:h-[58vh] drop-shadow-md/20 mt-4 aspect-auto">
