4f5460159ee6b2eb05ced6cddef32cadc77973c2
Michal Szyszlo <mszyszlo@student.agh.edu.pl>
2025-10-08 11:17:52 +0200

    Changes - v2

--- src/components/home/Profile/Profil2.js
+++ src/components/home/Profile/Profil2.js
@@ -2,12 +2,13 @@
 
 import {motion} from "motion/react"
 import {useState} from "react";
+import parse from 'html-react-parser';
 
 export default function Profill({key, item, d, tint}) {
-
 	const [hovered, setHovered] = useState(false);
+	const currentColor = item["Kolor"];
 	return (
-		<motion.div className="relative w-full h-full bg-zinc-100 ring-slate-300 ring-1 rounded-xl overflow-hidden"
+		<motion.div className="relative w-full h-full bg-zinc-100 ring-slate-300 ring-1 rounded-xl overflow-hidden select-none"
 		            onPointerEnter={() => setHovered(true)}
 		            onPointerLeave={() => setHovered(false)}
 		            onFocus={() => setHovered(true)}
@@ -17,11 +18,8 @@ export default function Profill({key, item, d, tint}) {
                       <span className="inline-flex items-center rounded-full bg-white px-5 py-2 text-[11px] sm:text-xs md:text-sm font-semibold text-zinc-500">
                         Profil
                       </span>
-				<div className="flex w-25 h-25 items-center justify-center m-3">
-					<svg xmlns="http://www.w3.org/2000/svg" className="h-full" viewBox="0 0 24 24"
-						 fill="none" stroke="#f00" strokeWidth={1.5}
-						 dangerouslySetInnerHTML={{ __html: item["IconPath"] }}
-					/>
+				<div className="flex w-25 h-25 items-center justify-center m-3" >
+					<div className={`w-full h-full`} style={{color: currentColor}} dangerouslySetInnerHTML={{ __html: item["IconPath"] }} />
 				</div>
 			</div>
 
