d67bf625a1c697e927bd380afdb766d18ab399e4
snappify <snappifyacc@gmail.com>
2025-10-09 08:39:32 +0200

    baner fix

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -7,9 +7,9 @@ import ButtonsBanner from "@/components/home/banner/buttonsBaner";
 export default function Banner() {
     return (<>
 		    <div className="relative w-full h-max md:h-max flex flex-col pt-[6rem] lg:pt-[7rem] items-center font-[poppins]">
-			    <div className="relative w-[94%] sm:w-[90%] lg:w-[80%] h-max flex items-center">
+			    <div className="relative w-full h-max flex items-center">
 				    <div className="w-full h-max flex items-center gap-2 lg:gap-10 py-1 justify-between">
-					    <Image src={logo} alt="logo" className="object-contain select-none max-h-15 max-w-15 md:max-h-20 md:max-w-20 lg:max-h-25 lg:max-w-25 drop-shadow-md/20"/>
+					    <Image src={logo} alt="logo" className="object-contain select-none max-h-15 max-w-15 md:max-h-20 md:max-w-20 lg:max-h-25 lg:max-w-25 drop-shadow-md/20 border-black"/>
 					    <p className=" text-center text-black/80  font-[poppins] text-sm sm:text-lg md:text-lg lg:text-xl xl:text-2xl font-medium break-words hyphens-auto">I Liceum Ogólnokształcące im. Jana Zamoyskiego w Zamościu </p>
 					    <Image src={godlo} priority alt="Godło" className="object-contain select-none max-h-15 max-w-15 md:max-h-20 md:max-w-20 lg:max-h-25 lg:max-w-25 " />
 				    </div>
