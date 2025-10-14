20acd1c5316b089b0d5e3e272c4ede1687f01872
Michal Szyszlo <mszyszlo@student.agh.edu.pl>
2025-10-14 13:09:55 +0200

    Małe poprawki

--- src/components/home/Profile/Profile.js
+++ src/components/home/Profile/Profile.js
@@ -2,7 +2,7 @@ import Profill from "@/components/home/Profile/Profil2";
 
 const Profile = ({data}) => {
     return (
-        <div className="font-[poppins] mt-5 z-10 w-full h-max flex px-6 py-6 md:px-10 md:py-8 bg-white rounded-2xl shadow-lg/20">
+        <div className="font-[poppins] z-10 w-full h-max flex px-6 py-6 md:px-10 md:py-8 bg-white rounded-2xl shadow-lg/20">
             <div className="h-max w-full flex flex-col gap-6 text-wrap md:items-start ">
                 <div className="flex flex-col w-max text-wrap">
                     <p className="text-base md:text-lg lg:text-xl font-normal text-slate-900">NASZE PROFILE</p>
