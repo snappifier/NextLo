5e02c9457cd99f1ec6ee17c3bea0073c612504f8
snappify <snappifyacc@gmail.com>
2025-10-16 20:07:18 +0200

    fix

--- src/app/galeria/ButtonAnimation.js
+++ src/app/galeria/ButtonAnimation.js
@@ -4,7 +4,7 @@ import {motion} from "motion/react"
 export default function ButtonAnimation({title}) {
 
 	return (
-		<motion.div whileHover={{scale: 1.05}} transition={{duration: 0.2, ease: "easeInOut"}} className=" h-max bg-[#3077BA] rounded-md flex items-center justify-center gap-2 text-white overflow-hidden">
+		<motion.div whileHover={{scale: 1.05}} transition={{duration: 0.2, ease: "easeInOut"}}  className=" h-max bg-[#3077BA] rounded-md flex items-center justify-center gap-2 text-white overflow-hidden">
 			<p className="px-10 py-5 text-xl text-wrap text-center hyphens-auto">{title}</p>
 		</motion.div>
 	)
