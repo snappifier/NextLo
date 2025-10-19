c1d7181f5c1374ebf3bbe7e299574403a98c6ab6
snappify <snappifyacc@gmail.com>
2025-10-19 18:39:24 +0200

    baner fix

--- src/components/home/banner/Banner.js
+++ src/components/home/banner/Banner.js
@@ -31,7 +31,7 @@ export default function Banner({baner}) {
 						className="object-cover rounded-2xl brightness-70"
 						priority
 						quality={85} // obniż z 100 (domyślne) do 85
-						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 90vw, 80vw"
+						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 90vw, 80vw "
 					/>
 				</div>
 				<div className="flex w-full h-full justify-center items-center py-5 rounded-b-2xl">
