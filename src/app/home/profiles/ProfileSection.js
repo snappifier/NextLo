890e1d5a591c267198c3b1facfc68a95a44d1e4a
snappifier <snappifyacc@gmail.com>
2026-01-02 23:17:48 +0100

    profiles bg change

--- src/app/home/profiles/ProfileSection.js
+++ src/app/home/profiles/ProfileSection.js
@@ -5,7 +5,7 @@ import Profile from "./Profile"
 const ProfileSection = ({data}) => {
     return (
         <div className=" w-full h-max relative overflow-hidden rounded-2xl shadow-lg/20 z-10">
-            <div className="absolute inset-0 bg-linear-to-br from-blue-100 via-sky-100 to-indigo-50" />
+            <div className="absolute inset-0 bg-linear-to-br from-blue-100 via-sky-50 to-indigo-50" />
             <div className="absolute inset-0 bg-[radial-gradient(ellipse_at_top_right,var(--tw-gradient-stops))] from-sky-100/50 via-transparent to-transparent" />
             <div className="relative px-6 py-6 md:px-10 md:py-8">
                 <div className="h-max w-full flex flex-col gap-6 text-wrap md:items-start">
