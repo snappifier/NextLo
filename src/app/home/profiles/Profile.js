53799d27f3a8a96eeb94718daab4563ddb1da3e3
snappifier <snappifyacc@gmail.com>
2026-01-02 23:15:43 +0100

    profiles fix

--- src/app/home/profiles/Profile.js
+++ src/app/home/profiles/Profile.js
@@ -63,7 +63,7 @@ export default function Profile({item}) {
 								 animate={hovered ? "hovered" : "idle"}
 								 transition={{duration: 0.2}}
 					>
-						<svg xmlns="http://www.w3.org/2000/svg" className="transition-transform duration-300 group-hover:translate-x-1" width={20} height={20} viewBox="0 0 24 24"><path fill="currentColor" d="M13.292 12L9.046 7.754q-.14-.14-.15-.344t.15-.364t.354-.16t.354.16l4.388 4.389q.131.13.184.267t.053.298t-.053.298t-.184.268l-4.388 4.388q-.14.14-.344.15t-.364-.15t-.16-.354t.16-.354z"></path></svg>
+						<svg xmlns="http://www.w3.org/2000/svg"  width={20} height={20} viewBox="0 0 24 24"><path fill="currentColor" d="M13.292 12L9.046 7.754q-.14-.14-.15-.344t.15-.364t.354-.16t.354.16l4.388 4.389q.131.13.184.267t.053.298t-.053.298t-.184.268l-4.388 4.388q-.14.14-.344.15t-.364-.15t-.16-.354t.16-.354z"></path></svg>
 
 					</motion.span>
 				</motion.div>
