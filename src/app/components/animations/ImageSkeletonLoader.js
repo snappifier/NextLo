c5b45dbf1176fb6af9d2d2691e337cd8da397abf
snappifier <snappifyacc@gmail.com>
2026-01-02 14:28:15 +0100

    add skeleton loader banner/about photos

---
+++ src/app/components/animations/ImageSkeletonLoader.js
@@ -0,0 +1,35 @@
+'use client'
+
+import {useState} from "react"
+import Image from "next/image"
+import {motion, AnimatePresence} from "motion/react"
+
+export default function ImageSkeletonLoader({src, alt, className, sizes}) {
+    const [isLoading, setIsLoading] = useState(true)
+
+    return (
+        <div className="relative w-full h-full">
+            <AnimatePresence>
+                {isLoading && (
+                    <motion.div className="absolute inset-0 z-10 rounded-2xl overflow-hidden bg-slate-300 select-none pointer-events-none"
+                                initial={{opacity: 1}}
+                                exit={{opacity: 0}}
+                                transition={{duration: 0.3, ease: "easeOut"}}
+                    >
+                        <div className="absolute inset-0 bg-linear-to-r from-slate-300 via-slate-200 to-slate-300 animate-shimmer" />
+                    </motion.div>
+                )}
+            </AnimatePresence>
+
+            <Image className={className}
+                   src={src}
+                   alt={alt}
+                   fill
+                   priority
+                   quality={85}
+                   sizes={sizes}
+                   onLoad={() => setIsLoading(false)}
+            />
+        </div>
+    )
+}
--- src/app/home/about/About.js
+++ src/app/home/about/About.js
@@ -1,16 +1,10 @@
-import Image from "next/image";
 import {getStrapiMedia} from "@/app/lib/strapi";
+import ImageSkeletonLoader from "@/app/components/animations/ImageSkeletonLoader";
 
 export default function About({data}) {
     return (
         <div className="font-poppins flex relative w-full justify-center ">
-            <section
-                className="
-          px-6 py-6 md:px-10 md:py-8
-          flex flex-col md:flex-row items-stretch gap-6 md:gap-10
-          bg-white rounded-2xl shadow-lg/20
-        "
-            >
+            <section className="px-6 py-6 md:px-10 md:py-8 flex flex-col md:flex-row items-stretch gap-6 md:gap-10 bg-white rounded-2xl shadow-lg/20">
                 <div className="flex-1 min-w-0">
                     <div className="flex flex-col w-max">
                         <p className="text-base md:text-lg lg:text-xl font-normal text-slate-900">KRÓTKO O SZKOLE</p>
@@ -28,16 +22,17 @@ export default function About({data}) {
                 </div>
 
                 {data?.["Zdjecie"] && (
-                    <div className="relative w-full max-w-[420px] h-[440px] aspect-auto md:h-[480px] flex-shrink-0 rounded-2xl overflow-hidden">
-                        <Image
-                            src={getStrapiMedia(data["Zdjecie"].url)}
-                            alt={data["Naglowek"] || "Zdjęcie"}
-                            fill
-                            loading="lazy"
-                            decoding="async"
-                            className="object-cover object-top"
-                            priority={false}
-                        />
+                    <div className="relative w-full max-w-105 h-110 aspect-auto md:h-120 shrink-0 rounded-2xl overflow-hidden">
+                        {/*<Image*/}
+                        {/*    src={getStrapiMedia(data["Zdjecie"].url)}*/}
+                        {/*    alt={data["Naglowek"] || "Zdjęcie"}*/}
+                        {/*    fill*/}
+                        {/*    loading="lazy"*/}
+                        {/*    decoding="async"*/}
+                        {/*    className="object-cover object-top"*/}
+                        {/*    priority={false}*/}
+                        {/*/>*/}
+                        <ImageSkeletonLoader src={getStrapiMedia(data["Zdjecie"].url)} alt={data["Naglowek"] || "Zdjecie"} className="object-cover object-top select-none pointer-events-none" />
                     </div>
                 )}
             </section>
